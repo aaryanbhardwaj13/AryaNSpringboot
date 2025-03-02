69b7a466456c8defbf0fcf55deb3c6715b4f07c2
aaryanbhardwaj13 <aaryanbhardwaj13@gmail.com>
2025-03-02 14:26:54 +0530

    Initial commit - Add project structure and configuration files

---
+++ src/main/java/com/example/AryanApp/AryanAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.AryanApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AryanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AryanAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AryanApp/AryanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AryanApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AryanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
